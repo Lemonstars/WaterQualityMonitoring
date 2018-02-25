dc2efff5d1522f12dbfada2554c10d44d9dc1f50
Lemonstars <141250079@smail.nju.edu.cn>
2018-02-25 13:19:43 +0800

    [develop]android侧边栏

--- app/src/main/java/lxing14/software/edu/nju/cn/waterqualitymonitoring/module/map/MapActivity.java
+++ app/src/main/java/lxing14/software/edu/nju/cn/waterqualitymonitoring/module/map/MapActivity.java
@@ -44,23 +44,17 @@ public class MapActivity extends AppCompatActivity
         }
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_waterLevel) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_waterForce) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_waterQuality) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_floatingMaterial) {
 
         }
 
