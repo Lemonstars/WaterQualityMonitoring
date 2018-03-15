888788c37b44f9601592c66fb3788f08f4e2f978
Lemonstars <141250079@smail.nju.edu.cn>
2018-03-15 20:29:32 +0800

    [develop]日期选择控件

--- app/src/main/java/lxing14/software/edu/nju/cn/waterqualitymonitoring/module/waterQuality/WaterQualityActivity.java
+++ app/src/main/java/lxing14/software/edu/nju/cn/waterqualitymonitoring/module/waterQuality/WaterQualityActivity.java
@@ -1,15 +1,20 @@
 package lxing14.software.edu.nju.cn.waterqualitymonitoring.module.waterQuality;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.fantasy.doubledatepicker.DoubleDateSelectDialog;
+
 import lxing14.software.edu.nju.cn.waterqualitymonitoring.R;
-import lxing14.software.edu.nju.cn.waterqualitymonitoring.module.waterLevel.WaterLevelFragment;
-import lxing14.software.edu.nju.cn.waterqualitymonitoring.module.waterLevel.WaterLevelPresenter;
 import lxing14.software.edu.nju.cn.waterqualitymonitoring.util.ActivityUtil;
 
 public class WaterQualityActivity extends AppCompatActivity {
+
+    private DoubleDateSelectDialog mDoubleTimeSelectDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +25,18 @@ public class WaterQualityActivity extends AppCompatActivity {
         TextView toolBarTV = findViewById(R.id.toolbar);
         toolBarTV.setText(R.string.waterQuality);
 
+        ImageView date_iv = findViewById(R.id.date_iv);
+        date_iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCustomTimePicker();
+            }
+        });
+
         //generate the view and the presenter
         WaterQualityFragment waterQualityFragment = (WaterQualityFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.contentFrame);
-        if(waterQualityFragment == null){
+        if (waterQualityFragment == null) {
             waterQualityFragment = WaterQualityFragment.generateFragment();
             ActivityUtil.addFragmentToActivity(getSupportFragmentManager(),
                     waterQualityFragment, R.id.contentFrame);
@@ -31,4 +44,31 @@ public class WaterQualityActivity extends AppCompatActivity {
 
         new WaterQualityPresenter(waterQualityFragment);
     }
+
+    public void showCustomTimePicker() {
+        String allowedSmallestTime = "2017-1-1";
+        String allowedBiggestTime = "2020-12-31";
+        String defaultChooseDate = "2018-01-18";
+
+        if (mDoubleTimeSelectDialog == null) {
+            mDoubleTimeSelectDialog = new DoubleDateSelectDialog(this, allowedSmallestTime, allowedBiggestTime, defaultChooseDate);
+            mDoubleTimeSelectDialog.setOnDateSelectFinished(new DoubleDateSelectDialog.OnDateSelectFinished() {
+                @Override
+                public void onSelectFinished(String startTime, String endTime) {
+
+                }
+            });
+
+            mDoubleTimeSelectDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                @Override
+                public void onDismiss(DialogInterface dialog) {
+
+                }
+            });
+        }
+        if (!mDoubleTimeSelectDialog.isShowing()) {
+            mDoubleTimeSelectDialog.show();
+        }
+    }
+
 }
